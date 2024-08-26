9441df1e1557f7088dabc422fdb65cf0156a8d45
Ravi <ravi@yoda-pc.fritz.box>
2024-08-26 22:17:30 +0200

    cleanup

--- kernelModules/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/ravi/dw/repos/Exploring-Linux-RPI4/kernelModules/modules.order := {   echo /home/ravi/dw/repos/Exploring-Linux-RPI4/kernelModules/hello_world.o; :; } > /home/ravi/dw/repos/Exploring-Linux-RPI4/kernelModules/modules.order
